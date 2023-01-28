c363ecb9addb238f2bf8913928601122b4c8c955
jongin403 <jongin403@naver.com>
2023-01-29 03:53:15 +0900

    feat: legacyBehavior 사용

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,21 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link href="/">
-        <div className="link-name">Home</div>
+      <Link legacyBehavior href="/">
+        <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
       </Link>
-      <Link href="/about">
-        <div className="link-name">About</div>
+      <Link legacyBehavior href="/about">
+        <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
       </Link>
       <style jsx>{`
-        nav {
-          background-color: tomato;
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: tomato;
         }
       `}</style>
     </nav>
